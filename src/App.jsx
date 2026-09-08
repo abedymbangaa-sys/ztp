62246efa001bc8402d4cc4ff30f7365e08e778dc
abedymbangaa-sys <abedymbangaa@gmail.com>
2026-09-09 00:28:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ const Area = lazy(() => import("./pages/Area"));
 const Guides = lazy(() => import("./pages/Guides"));
 const GuideDetail = lazy(() => import("./pages/GuideDetail"));
 const TripBuilder = lazy(() => import("./pages/TripBuilder"));
+const FindLocalHost = lazy(() => import("./pages/FindLocalHost"));
 const Collections = lazy(() => import("./pages/Collections"));
 const CollectionDetail = lazy(() => import("./pages/CollectionDetail"));
 const NearMe = lazy(() => import("./pages/NearMe"));
@@ -108,6 +109,7 @@ export default function App() {
             <Route path="/guides" element={<Guides />} />
             <Route path="/guides/:slug" element={<GuideDetail />} />
             <Route path="/trip-builder" element={<TripBuilder />} />
+            <Route path="/find-host" element={<FindLocalHost />} />
             <Route path="/:sectionKey" element={<SectionListing />} />
             <Route path="/:sectionKey/:id" element={<SectionDetail />} />
           </Routes>
