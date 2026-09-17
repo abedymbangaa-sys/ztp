2170ae114e221a72ec10ca56d58b5d5b344afba8
abedymbangaa-sys <abedymbangaa@gmail.com>
2026-09-17 19:30:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,8 @@ const Area = lazy(() => import("./pages/Area"));
 const Guides = lazy(() => import("./pages/Guides"));
 const GuideDetail = lazy(() => import("./pages/GuideDetail"));
 const TripBuilder = lazy(() => import("./pages/TripBuilder"));
+const ReviewQuick = lazy(() => import("./pages/ReviewQuick"));
 const FindLocalHost = lazy(() => import("./pages/FindLocalHost"));
-const PhotoSpots = lazy(() => import("./pages/PhotoSpots"));
-const Sustainable = lazy(() => import("./pages/Sustainable"));
 const Collections = lazy(() => import("./pages/Collections"));
 const CollectionDetail = lazy(() => import("./pages/CollectionDetail"));
 const NearMe = lazy(() => import("./pages/NearMe"));
@@ -114,8 +113,7 @@ export default function App() {
             <Route path="/guides/:slug" element={<GuideDetail />} />
             <Route path="/trip-builder" element={<TripBuilder />} />
             <Route path="/find-host" element={<FindLocalHost />} />
-            <Route path="/photo-spots" element={<PhotoSpots />} />
-            <Route path="/sustainable" element={<Sustainable />} />
+            <Route path="/review/:id" element={<ReviewQuick />} />
             <Route path="/:sectionKey" element={<SectionListing />} />
             <Route path="/:sectionKey/:id" element={<SectionDetail />} />
           </Routes>
